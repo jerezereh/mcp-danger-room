e84a1c9f7dea7ca240a76fe79acde25b8affb1b3
Jerezereh <mavop01@outlook.com>
2021-10-04 20:10:04 -0500

    WIP: roster builder, character card browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import { GlobalStyle } from './styles/GlobalStyle'
+import { CharacterRow } from './components/CharacterRow/CharacterRow'
 
-import { Greetings } from './components/Greetings'
+import data from '../assets/CharacterCards.json'
 
 export function App() {
   return (
     <>
       <GlobalStyle />
-      <Greetings />
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Alter Ego</th>
+            <th>Affiliations</th>
+            <th>CP</th>
+            <th>Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.Characters.map((char) => {
+            return (<CharacterRow 
+              name={char.Name} 
+              alterEgo={char['Alter Ego']} 
+              affiliiations={char.Affiliations} 
+              cp={char.CP} 
+              cost={char.Cost}
+              />);
+          })}
+        </tbody>
+      </table>
+      {/* <InfoPane></InfoPane> */}
+      {/* <RosterView></RosterView> */}
     </>
   )
 }
---
+++ src/components/CharacterRow/CharacterRow.tsx
@@ -0,0 +1,13 @@
+import { Text } from './styles'
+import { ISimpleCharacterProps } from "../../dataTypes/ISimpleCharacterProps";
+
+export const CharacterRow = (props: ISimpleCharacterProps) => {
+  return (
+    <tr key={props.name}>
+      <td><Text>{props.name}</Text></td>
+      <td><Text>{props.alterEgo}</Text></td>
+      <td><Text>{props.affiliiations.join(', ')}</Text></td>
+      <td><Text>{props.cp}</Text></td>
+      <td><Text>{props.cost}</Text></td>
+    </tr>);
+}
