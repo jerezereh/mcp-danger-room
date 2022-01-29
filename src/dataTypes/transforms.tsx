22e9972fc9e25228824c793cd037b1f93fa24358
Maxx Spicer <mavop01@gmail.com>
2022-01-28 21:57:18 -0600

    Updated dependencies; using MUI v5 tables and styling; created CardTable as generic table component; added fields to ICardProps for CardTable view

--- src/dataTypes/transforms.tsx
+++ src/dataTypes/transforms.tsx
@@ -3,10 +3,12 @@ import { ICardProps } from "./ICardProps";
 export function CharacterDataToICard(character: any): ICardProps {
     return {
         name: character.Name,
+        alterEgo: character["Alter Ego"],
         type: "Character",
         cardImage1: character.Healthy.CardImage,
         cardImage2: character.Injured.CardImage,
         affiliations: character.Affiliations,
+        cost: character.Cost,
         cp: character.CP
     };
 }
