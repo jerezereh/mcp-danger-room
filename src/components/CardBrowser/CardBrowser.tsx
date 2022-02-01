37338bbb04a2f36cc8797973c6b2c5396a340620
Maxx Spicer <mavop01@gmail.com>
2022-02-01 14:20:50 -0600

    Added react-map-interaction for game board; added game view to App.tsx with state management (to be changed); refactored styling; created tab panel using MUI

--- src/components/CardBrowser/CardBrowser.tsx
+++ src/components/CardBrowser/CardBrowser.tsx
@@ -1,17 +1,13 @@
 import { BaseSyntheticEvent } from 'react';
 import data from '../../../assets/CharacterCards.json';
-import { CharacterDataToICard } from "../../dataTypes/transforms";
-import { CardTable } from "./CardTable";
+import { CharacterDataToICard } from '../../dataTypes/transforms';
+import { CardTable } from './CardTable';
 
-export function CardBrowser(props: { setSelectedCard: any; }) {
-  // const columns = ["Name", "Alter Ego", "Affiliations", "Cost", "CP"];
-  // const options = {
-  //   filter: true,
-  //   onRowClick: onRowClick,
-  // };
-
+export function CardBrowser(props: { setSelectedCard: any }) {
   function onRowClick(event: BaseSyntheticEvent) {
-    const result = data.Characters.find(c => c.Name === event.target.parentNode.childNodes[0].outerText);
+    const result = data.Characters.find(
+      c => c.Name === event.target.parentNode.childNodes[0].outerText
+    );
     if (result === undefined) {
       return;
     }
@@ -21,6 +17,10 @@ export function CardBrowser(props: { setSelectedCard: any; }) {
 
   return (
     <>
-      <CardTable dataSet={data.Characters.map(CharacterDataToICard)} onRowClick={onRowClick} />
-    </>);
+      <CardTable
+        dataSet={data.Characters.map(CharacterDataToICard)}
+        onRowClick={onRowClick}
+      />
+    </>
+  );
 }
